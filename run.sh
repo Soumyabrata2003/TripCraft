e475c3f38813d6f35955d4481d1f2cc1c54840c7
Soumyabrata2003 <110310286+Soumyabrata2003@users.noreply.github.com>
2025-05-25 18:40:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export MODEL_NAME="gpt-4o"     #qwen    #phi4             # Model name (you can 
 export OPENAI_API_KEY="YOUR_OPENAI_KEY"             # Your OpenAI API key
 # export GOOGLE_API_KEY="YOUR_GOOGLE_KEY"                 # Your Google API key
 export SET_TYPE="3day_gpt4o_orig"                            # Set type- name of folder in O/P directory where generated outputs get saved 
-export STRATEGY="direct_og"                                # direct_parameter_info / direct_og
+export STRATEGY="direct_og"                                # direct_og / direct_param
 export CSV_FILE="/home/soumya/TripCraft/final_3day_dataset.csv"  # Path to your CSV file
 
 # Navigate to the planner directory
